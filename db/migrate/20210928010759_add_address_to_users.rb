03f963abe1fe9bbb03f8f234fec8d6f8128f6e35
elijahsamuels <elijahsamuels@gmail.com>
2021-09-27 19:35:21 -0600

    first commit

---
+++ db/migrate/20210928010759_add_address_to_users.rb
@@ -0,0 +1,9 @@
+class AddAddressToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :address1, :string
+    add_column :users, :address2, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+    add_column :users, :zip_code, :string
+  end
+end
