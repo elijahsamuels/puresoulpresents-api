97e764726dbedf71c2c8f381dc12e994b77d3e48
elijahsamuels <elijahsamuels@gmail.com>
2021-11-05 13:08:41 -0600

    added columns on events table for musician pay rate, invoice received and invoice sent

---
+++ db/migrate/20211105185455_add_musician_invoice_paid_invoice_recevied_pay_rate.rb
@@ -0,0 +1,64 @@
+class AddMusicianInvoicePaidInvoiceReceviedPayRate < ActiveRecord::Migration[6.1]
+  def change
+    add_column :events, :musician_1_pay_rate, :integer
+    add_column :events, :musician_1_invoice_paid, :boolean
+    add_column :events, :musician_1_invoice_received, :boolean
+    
+    add_column :events, :musician_2_pay_rate, :integer
+    add_column :events, :musician_2_invoice_paid, :boolean
+    add_column :events, :musician_2_invoice_received, :boolean
+    
+    add_column :events, :musician_3_pay_rate, :integer
+    add_column :events, :musician_3_invoice_paid, :boolean
+    add_column :events, :musician_3_invoice_received, :boolean
+    
+    add_column :events, :musician_4_pay_rate, :integer
+    add_column :events, :musician_4_invoice_paid, :boolean
+    add_column :events, :musician_4_invoice_received, :boolean
+    
+    add_column :events, :musician_5_pay_rate, :integer
+    add_column :events, :musician_5_invoice_paid, :boolean
+    add_column :events, :musician_5_invoice_received, :boolean
+    
+    add_column :events, :musician_6_pay_rate, :integer
+    add_column :events, :musician_6_invoice_paid, :boolean
+    add_column :events, :musician_6_invoice_received, :boolean
+    
+    add_column :events, :musician_7_pay_rate, :integer
+    add_column :events, :musician_7_invoice_paid, :boolean
+    add_column :events, :musician_7_invoice_received, :boolean
+    
+    add_column :events, :musician_8_pay_rate, :integer
+    add_column :events, :musician_8_invoice_paid, :boolean
+    add_column :events, :musician_8_invoice_received, :boolean
+    
+    add_column :events, :musician_9_pay_rate, :integer
+    add_column :events, :musician_9_invoice_paid, :boolean
+    add_column :events, :musician_9_invoice_received, :boolean
+    
+    add_column :events, :musician_10_pay_rate, :integer
+    add_column :events, :musician_10_invoice_paid, :boolean
+    add_column :events, :musician_10_invoice_received, :boolean
+    
+    add_column :events, :musician_11_pay_rate, :integer
+    add_column :events, :musician_11_invoice_paid, :boolean
+    add_column :events, :musician_11_invoice_received, :boolean
+    
+    add_column :events, :musician_12_pay_rate, :integer
+    add_column :events, :musician_12_invoice_paid, :boolean
+    add_column :events, :musician_12_invoice_received, :boolean
+    
+    add_column :events, :musician_13_pay_rate, :integer
+    add_column :events, :musician_13_invoice_paid, :boolean
+    add_column :events, :musician_13_invoice_received, :boolean
+    
+    add_column :events, :musician_14_pay_rate, :integer
+    add_column :events, :musician_14_invoice_paid, :boolean
+    add_column :events, :musician_14_invoice_received, :boolean
+    
+    add_column :events, :musician_15_pay_rate, :integer
+    add_column :events, :musician_15_invoice_paid, :boolean
+    add_column :events, :musician_15_invoice_received, :boolean
+
+  end
+end
