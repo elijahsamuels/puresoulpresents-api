6c36b233529cc06648b49bf9665065809c0d3d42
elijahsamuels <elijahsamuels@gmail.com>
2021-11-11 19:57:22 -0700

    added serializer for user model

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,9 @@
+class UserSerializer < ActiveModel::Serializer
+# class UserSerializer < ApplicationSerializer
+  attributes :id,  :first_name, :last_name, :email, :phone, :address1, :address2, :city, :state, :zip_code, :account_name, :ach_number, :ach_routing_number, :bio, :nick_name, :staff_notes, :user_staff_rating, :photo, :tax_first_or_business_name, :tax_last_name, :tax_address1, :tax_address2, :tax_city, :tax_state, :tax_zip, :taxID, :created_at,  :updated_at
+  
+  has_many :gigs
+  has_many :events, through: :gigs
+
+  
+end
